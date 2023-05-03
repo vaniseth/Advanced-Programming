8d293c9ffb5720cf6f3db6bf93f2244f90ef3fc7
vaniseth <74897823+vaniseth@users.noreply.github.com>
2023-05-04 01:38:41 +0530

    gdc

---
+++ Recursion/GCD using Euclid's Algo.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int gcd(int a, int b){
+    if (a == 0)
+    return b;
+    return gcd(b % a, a);
+}
+int main(){
+    int a = 10, b = 15;
+    printf("GCD(%d, %d) = %d\n", a, b, gcd(a, b));
+    a = 35, b = 10;
+    printf("GCD(%d, %d) = %d\n", a, b, gcd(a, b));
+    a = 31, b = 2;
+    printf("GCD(%d, %d) = %d\n", a, b, gcd(a, b));
+    return 0;
+}
